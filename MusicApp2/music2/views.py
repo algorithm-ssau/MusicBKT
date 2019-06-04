0706ec146d7f1d1416f9f94d549c7aff23a27589
donnaadrianna <48478005+donnaadrianna@users.noreply.github.com>
2019-06-04 05:03:11 +0400

    Add files via upload

--- MusicApp2/music2/views.py
+++ MusicApp2/music2/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index (request):
     return render (request,'music2/index.html')
+
+def contact(request):
+
+    return render(request, 'mysic2/basic.html', {'values':['jgkfjgkfjgfjkgf','456787654']})
